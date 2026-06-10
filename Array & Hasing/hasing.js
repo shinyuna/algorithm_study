b9b5ded3e1a999efa9cf7d569a84c1b96deeaa22
yunadev <yunadev01@gmail.com>
2026-06-10 16:07:56 +0900

    🌱

---
+++ Array & Hasing/hasing.js
@@ -0,0 +1,97 @@
+/**
+ * Hashing problems
+ */
+class Solution {
+  hasDuplicate(nums) {
+    const map = new Map();
+
+    for (const num of nums) {
+      if (map.has(num)) {
+        return true;
+      }
+
+      map.set(num, num);
+    }
+
+    return false;
+  }
+
+  /**
+   * 시간 복잡도: O(n)
+   * 공간 복잡도: O(n)
+   */
+  isAnagram(s, t) {
+    if (s.length !== t.length) {
+      return false;
+    }
+
+    const words = new Map();
+    for (const str of s) {
+      if (words.has(str)) {
+        words.set(str, words.get(str) + 1);
+      } else {
+        words.set(str, 1);
+      }
+    }
+
+    for (const str of t) {
+      if (!words.has(str)) {
+        return false;
+      }
+
+      const count = words.get(str);
+      if (count === 1) {
+        words.delete(str);
+      } else {
+        words.set(str, count - 1);
+      }
+    }
+
+    return words.size === 0;
+  }
+
+  twoSum(nums, target) {
+    const numToIndex = new Map();
+
+    for (let i = 0; i < nums.length; i++) {
+      const diff = target - nums[i];
+      if (numToIndex.has(diff)) {
+        return [numToIndex.get(diff), i];
+      }
+
+      numToIndex.set(nums[i], i);
+    }
+  }
+
+  groupAnagrams(strs) {
+    const result = [];
+    const anagramGroupKey = new Map();
+
+    let keyCount = 0;
+    for (const str of strs) {
+      const sorted = str.split("").sort().join("");
+
+      if (anagramGroupKey.has(sorted)) {
+        result[anagramGroupKey.get(sorted)].push(str);
+        continue;
+      }
+
+      anagramGroupKey.set(sorted, keyCount);
+      result[keyCount] = [str];
+      keyCount++;
+    }
+
+    return result;
+  }
+}
+
+const solution = new Solution();
+
+console.log(solution.hasDuplicate([1, 2, 3, 4])); // false
+console.log(solution.hasDuplicate([1, 2, 3, 1])); // true
+
+console.log(solution.isAnagram("anagram", "nagaram")); // true
+console.log(solution.isAnagram("rat", "car")); // false
+
+console.log(solution.twoSum([2, 7, 11, 15], 9)); // [0, 1]
+console.log(solution.twoSum([3, 4, 5, 6], 7)); // [0, 1]
